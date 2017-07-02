d3d3f8e250c0d3b2ae78741b1968e0dabb242075
samfun123 <samdinner@hotmail.com>
2017-07-02 02:39:30 -0400

    Updated Boolean Venn Diagrams logging.

--- More/Logfile Analyzer.js
+++ More/Logfile Analyzer.js
@@ -84,6 +84,29 @@ var blacklist = [
 	"Calculated FPS: "
 ];
 
+// https://stackoverflow.com/a/21420210/8213163
+String.prototype.indexOfRegex = function(regex, startpos) {
+	var indexOf = this.substring(startpos || 0).search(regex);
+	return (indexOf >= 0) ? (indexOf + (startpos || 0)) : indexOf;
+};
+
+String.prototype.lastIndexOfRegex = function(regex, startpos) {
+	regex = (regex.global) ? regex : new RegExp(regex.source, "g" + (regex.ignoreCase ? "i" : "") + (regex.multiLine ? "m" : ""));
+	if(typeof (startpos) == "undefined") {
+		startpos = this.length;
+	} else if(startpos < 0) {
+		startpos = 0;
+	}
+	var stringToWorkWith = this.substring(0, startpos + 1);
+	var lastIndexOf = -1;
+	var nextStop = 0;
+	while((result = regex.exec(stringToWorkWith)) != null) {
+		lastIndexOf = result.index;
+		regex.lastIndex = ++nextStop;
+	}
+	return lastIndexOf;
+};
+
 // Search for 'var lineRegex' for the list of all the line matching regex & the reference.
 
 $(function() {
@@ -2465,7 +2488,38 @@ $(function() {
 						regex: /Expression/,
 						value: function(matches, module) {
 							readLine();
-							module.push({ label: "Diagram:", obj: pre(readMultiple(16)), expanded: true });
+
+							var svg = $('<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 348 348"><g transform="matrix(1.260512,0,0,1.260512,-59.613368,-83.043883)" fill="#fff" stroke="#000" stroke-width="3"><path d="m254 179.9c-11.8-6.8-25.5-10.6-40-10.6-14.7 0-28.4 3.9-40.2 10.8-11.8-6.9-25.6-10.8-40.2-10.8-14.8 0-28.6 4-40.5 10.9 0-44.4 36.1-80.5 80.5-80.5 44.3 0 80.3 35.9 80.4 80.2z" fill="rgb(127, 255, 127)"></path><circle cx="280" cy="110" r="24" fill="rgb(127, 255, 127)"></circle><path d="m173.7 180.1c-0.1 0.1-0.2 0.1-0.3 0.2-23.9 14-40 39.9-40 69.6v0.3l-0.1-0.1c-24.1-13.9-40.3-40-40.3-69.8v-0.1c11.9-6.9 25.7-10.9 40.5-10.9 14.6 0 28.4 3.9 40.2 10.8z" fill="rgb(127, 255, 127)"></path><path d="m254 180.2c0 29.7-16.1 55.7-40 69.6v-0.1-0.3c-0.1-29.6-16.1-55.4-40-69.3-0.1-0.1-0.2-0.1-0.3-0.2 11.8-6.9 25.6-10.8 40.2-10.8s28.2 3.9 40 10.6c0.1 0.3 0.1 0.4 0.1 0.5z" fill="rgb(255, 127, 127)"></path><path d="m214 249.5c-0.1 0.2-0.2 0.3-0.3 0.5-11.8 6.8-25.5 10.7-40.2 10.7-14.6 0-28.2-3.9-40-10.6v-0.3c0-29.7 16.1-55.6 40-69.6h0.1 0.5c23.7 14 39.8 39.7 39.9 69.3z" fill="rgb(255, 127, 127)"></path><path d="m173.7 319.5c-11.8 6.9-25.6 10.8-40.2 10.8-44.5 0-80.5-36-80.5-80.5 0-29.7 16.1-55.7 40-69.6v0.1c0 29.8 16.2 55.9 40.3 69.8 0 0.1 0.1 0.1 0.1 0.2 0.2 29.6 16.4 55.4 40.3 69.2z" fill="rgb(127, 255, 127)"></path><path d="m294.5 249.8c0 44.5-36.1 80.5-80.5 80.5-14.7 0-28.4-3.9-40.2-10.8 24-13.9 40.2-39.9 40.2-69.7 24-14 40-39.9 40-69.6v-0.3c24.2 13.9 40.5 40 40.5 69.9z" fill="rgb(127, 255, 127)"></path><path d="m214 249.9c0 29.8-16.2 55.8-40.2 69.7-23.9-13.8-40.1-39.7-40.2-69.3v-0.1c11.8 6.8 25.5 10.6 40 10.6 14.6 0 28.4-3.9 40.2-10.7 0-0.1 0.1-0.2 0.2-0.2z" fill="rgb(255, 127, 127)"></path></g></svg>')
+							.appendTo("body");
+
+							var sections = ["A", "NONE", "AB", "AC", "ABC", "B", "C", "BC"];
+							var positions = {
+								"AB": "translate(125px, 200px)",
+								"AC": "translate(225px, 200px)",
+								"B": "translate(93px, 275px)",
+								"C": "translate(254px, 275px)"
+							};
+
+							readMultiple(16).match(/[UL]/g).forEach(function(letter, index) {
+								var elem = svg.find("path, circle").eq(index);
+								elem.attr("fill", letter == "L" ? "rgb(255, 127, 127)" : "rgb(127, 255, 127)");
+
+								var bbox = elem[0].getBBox();
+								var text = sections[index];
+								$("<svg><text></svg>").children().eq(0).unwrap()
+								.text(text)
+								.css({
+									transform: positions[text] || "translate(" + (bbox.x + bbox.width / 2) + "px, " + (bbox.y + bbox.height / 2) + "px)",
+									"text-anchor": "middle",
+    								"dominant-baseline": "central",
+									stroke: "none",
+    								fill: "black",
+    								"font-size": (35 - text.length * 5) + "px"
+								})
+								.appendTo(svg.children("g"));
+							});
+
+							module.push({ label: "Diagram:", obj: $("<div style='width: 40%'>").append(svg), expanded: true });
 						}
 					}
 				]
