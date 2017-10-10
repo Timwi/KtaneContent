e5f0ab973d2bd98f0c9677f687cffdeebb909a78
Timwi <github@timwi.de>
2017-10-10 02:35:43 +0200

    Fix highlighter on mobile.

--- HTML/js/highlighter.js
+++ HTML/js/highlighter.js
@@ -19,7 +19,10 @@ e.onload = function()
 	{
 		var enabled = true;
 		var colors = ["rgba(68, 130, 255, 0.4)", "rgba(223, 32, 32, 0.4)", "rgba(34, 195, 34, 0.4)", "rgba(223, 223, 32, 0.4)"];
-		var currentColor = colors[0];
+		var currentColor;
+		var setColor = function(color) {
+			currentColor = colors[color];
+		};
 
 		var highlighterAlert = $('<div style="position: fixed;top: 0;left: 0;padding: 6px;color: black;border-bottom:1px rgba(0, 0, 0, 0.5) solid;border-right:1px rgba(0, 0, 0, 0.5) solid;border-bottom-right-radius:5px;">Highlighter Color</div>').appendTo("body").hide();
 		var alertTimeout;
@@ -32,60 +35,78 @@ e.onload = function()
 		}).keydown(function(event) {
 			if (event.altKey && event.keyCode >= 49 && event.keyCode <= 53)
 			{
-				currentColor = colors[event.keyCode - 49];
+				setColor(event.keyCode - 49);
 
 				if (alertTimeout) {
 					clearTimeout(alertTimeout);
 				}
 
-				highlighterAlert.css("background-color", currentColor).fadeIn(500);
+				highlighterAlert.css("background-color", colors[currentColor]).fadeIn(500);
 				alertTimeout = setTimeout(function() {
 					highlighterAlert.fadeOut(500);
 				}, 2000);
 			}
 		});
 
-		$("<style> .ktane-highlight { background-color: rgba(68, 130, 255, 0.4); position: absolute; } </style>").appendTo($("head"));
-
 		var mode = null;
 		var mobileControls = false;
 		if ((/mobi/i.test(navigator.userAgent) || window.location.hash.indexOf('highlight') !== -1) && window.location.hash.indexOf('nohighlight') === -1)
 		{
-			$(
-				"<style>" +
-				"   .ktane-highlight-btn { position: fixed; top: 5px; width: 50px; height: 50px; opacity: .2; border-radius: 10px; }" +
-				"   .ktane-highlight-btn::after { content: ''; position: absolute; top: 0; left: 0; right: 0; bottom: 0; background: url('data:image/svg+xml,<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 1000 1000\"><path d=\"M795.4 36.92c-4.4 2.49-136.55 116.25-293.78 252.8-247.82 215.45-291.3 253.95-328.07 291.1-56.69 57.26-55.54 53.62-36.96 115.48 4.98 16.85 6.32 24.51 6.32 38.3.38 33.51-5.94 42.9-74.31 111.65-29.69 29.88-54.77 56.11-56.11 58.41-1.34 2.3-2.3 5.75-2.49 7.66 0 5.17 5.36 14.36 9.96 16.85 9 4.79 125.44 38.11 133.48 38.11 8.24 0 8.62-.38 43.09-34.09 36.39-35.62 46.92-43.47 67.03-49.22 17.81-5.17 43.67-3.26 76.8 5.75 29.3 7.85 42.33 8.23 54.77 1.92 3.64-1.92 24.13-20.68 45.58-41.94 32.56-32.17 79.67-85.22 287.85-324.81 137.12-157.42 251.46-290.14 254.52-294.74 7.28-11.11 8.81-25.09 4.6-39.64-3.26-11.11-4.79-12.83-71.43-79.86-76.22-76.42-78.14-77.95-99.97-77.95-9.58 0-14.75 1.15-20.88 4.22z\"/><path d=\"M885.8 135.55c57.45 57.45 64.54 65.31 64.16 70.09-.38 4.02-60.13 74.12-239.01 279.99L472.51 760.27 367.56 655.51c-57.65-57.65-104.57-105.33-104.38-105.91.38-.77 124.29-108.59 275.4-239.97C704.62 165.42 814.94 71 817.23 71c2.29 0 27.01 23.18 68.57 64.55zM412.18 822.7c-26.81 26.81-34.47 33.51-38.69 33.51-2.87 0-17.24-2.87-31.98-6.51-14.56-3.64-32.75-7.28-40.03-7.85-27-2.68-62.63 5.75-83.5 19.73-4.98 3.45-9.58 6.13-10.34 6.13-.77 0-13.22-11.87-27.77-26.43l-26.24-26.43 4.98-6.51c8.23-11.11 17.81-31.79 21.07-45.58 5.17-21.26 2.87-49.03-6.7-81.78-4.21-14.75-7.85-28.92-7.85-31.6 0-3.64 8.04-12.83 33.9-38.69l34.09-34.09 106.29 106.3L445.7 789.19l-33.52 33.51zM153.25 870.2l25.28 25.28-15.7 15.71-15.9 15.7-39.83-11.69-39.84-11.87 29.3-29.3c16.09-16.09 29.69-29.3 30.26-29.3.57 0 12.45 11.49 26.43 25.47z\" fill=\"%2377a4ff\"/></svg>') center center no-repeat; }" +
-				"   .ktane-highlight-btn.column { background: url('data:image/svg+xml,<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 10 10\"><path d=\"M 4 0 6 0 6 10 4 10 z\" fill=\"%234482ff\"/></svg>') center center no-repeat; }" +
-				"   .ktane-highlight-btn.row { background: url('data:image/svg+xml,<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 10 10\"><path d=\"M 0 6 0 4 10 4 10 6 z\" fill=\"%234482ff\"/></svg>') center center no-repeat; }" +
-				"   .ktane-highlight-btn.selected { background-color: #afa; opacity: .6; }" +
-				"</style>").appendTo($("head"));
+			var styleTag = $("<style>").appendTo($("head"));
+			setColor = function(color) {
+				currentColor = color;
+				styleTag.text((
+					".ktane-highlight-svg { width: 50px; height: 50px; }" +
+					".ktane-highlight-pen { fill: !0!; }" +
+					".ktane-highlight-btn { position: fixed; top: 5px; width: 50px; height: 50px; border: 3px solid transparent; border-radius: 10px; }" +
+					".ktane-highlight-btn.selected { border-color: #4c6; }")
+					.replace(/!0!/g, colors[color]));
+			}
+
+			var penSvg = "<svg xmlns='http://www.w3.org/2000/svg' viewBox='-50 -50 1100 1100' class='ktane-highlight-svg'>{0}<path d='M816.3 32.7c-9.6 0-14.8 1-21 4.2C791 39.3 659 153 501.7 289.6c-247.8 215.5-291.3 254-328 291-56.7 57.4-55.6 53.8-37 115.6 5 17 6.3 24.5 6.3 38.3.3 33.5-6 43-74.4 111.6-29.7 30-54.8 56.2-56 58.5-1.4 2.3-2.4 5.7-2.6 7.6 0 5.2 5.4 14.4 10 17 9 4.6 125.4 38 133.4 38 8.3 0 8.7-.4 43-34 36.5-35.7 47-43.6 67.2-49.3 17.8-5.2 43.6-3.3 76.8 5.7 29.3 8 42.3 8.2 54.7 2 3.8-2 24.3-20.8 45.7-42 32.6-32.2 79.7-85.2 288-324.8C865.6 367.4 980 234.7 983 230c7.4-11.2 9-25 4.7-39.7-3.3-11-4.8-12.8-71.5-79.8-76.2-76.5-78-78-100-78zm1 38.3c2.2 0 27 23.2 68.5 64.6 57.5 57.4 64.5 65.2 64.2 70-.4 4-60.2 74.2-239 280L472.4 760.3l-105-104.8C310 598 263 550.2 263.3 549.5c.4-.7 124.3-108.5 275.4-240C704.6 165.6 815 71 817.2 71zM233 576.6L339.5 683 445.7 789l-33.5 33.5c-26.8 26.8-34.5 33.5-38.7 33.5-3 0-17.3-3-32-6.5-14.6-3.7-32.8-7.3-40-8-27-2.5-62.7 6-83.5 20-5 3.3-9.6 6-10.4 6-.7 0-13.2-12-27.7-26.4l-26.4-26.5 5-6.5c8.3-11 17.8-31.8 21-45.5 5.3-21.3 3-49-6.6-81.8-4.2-14.8-8-29-8-31.6 0-3.7 8.2-13 34-38.7l34-34zM127 844.6c.6 0 12.5 11.6 26.5 25.6l25.2 25.3-15.7 15.7-16 15.7-39.7-11.8-39.8-12L96.7 874c16-16 29.6-29.3 30.2-29.3z' fill='black'/><path d='M885.8 135.6c57.5 57.4 64.5 65.3 64.2 70-.4 4-60.2 74.2-239 280L472.4 760.3l-105-104.8C310 598 263 550.2 263.3 549.5c.4-.7 124.3-108.5 275.4-240C704.6 165.6 815 71 817.2 71c2.3 0 27 23.2 68.6 64.5zm-473.6 687c-26.8 27-34.5 33.6-38.7 33.6-3 0-17.2-3-32-6.5-14.6-3.6-32.7-7.3-40-7.8-27-2.8-62.6 5.6-83.5 19.6-5 3.4-9.6 6-10.4 6-.7 0-13.2-11.8-27.7-26.3L153.5 815l5-6.7c8.2-11 17.8-31.7 21-45.5 5.2-21.3 3-49-6.6-81.8-4.2-14.8-8-29-8-31.6 0-3.7 8.2-12.8 34-38.7l34-34 106.4 106.2L445.7 789l-33.5 33.5zm-259 47.6l25.3 25.3-15.7 15.7-16 15.7-39.7-11.8-39.7-12L96.6 874c16-16 29.7-29.3 30.2-29.3.6 0 12.5 11.5 26.5 25.5z' class='ktane-highlight-pen'/></svg>";
 
 			if ($('td, th, li, .highlightable').length)
 			{
-				$('<div class="ktane-highlight-btn element">').data('mode', 'element').css({ right: 5 }).appendTo(document.body);
+				$('<div class="ktane-highlight-btn color">').data('mode', 'color').css({ right: 5 }).appendTo(document.body).append($((
+					"<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 10.5 10.5' class='ktane-highlight-svg'>" +
+					"<rect x='0.5' y='0.5' width='4.5' height='4.5' fill='!0!' rx='1' ry='1' />" +
+					"<rect x='5.5' y='0.5' width='4.5' height='4.5' fill='!1!' rx='1' ry='1' />" +
+					"<rect x='0.5' y='5.5' width='4.5' height='4.5' fill='!2!' rx='1' ry='1' />" +
+					"<rect x='5.5' y='5.5' width='4.5' height='4.5' fill='!3!' rx='1' ry='1' />" +
+					"</svg>").replace(/!(\d+)!/g, function(_, m) { return colors[+m]; })));
+				$('<div class="ktane-highlight-btn element">').data('mode', 'element').css({ right: 60 }).appendTo(document.body).append($(penSvg.replace(/\{0\}/g, '')));
 				if ($('td, th').length)
 				{
-					$('<div class="ktane-highlight-btn row">').data('mode', 'row').css({ right: 60 }).appendTo(document.body);
-					$('<div class="ktane-highlight-btn column">').data('mode', 'column').css({ right: 115 }).appendTo(document.body);
+					$('<div class="ktane-highlight-btn row">').data('mode', 'row').css({ right: 115 }).appendTo(document.body)
+						.append($(penSvg.replace(/\{0\}/g, "<path d='M 0 600 0 400 1000 400 1000 600 z' class='ktane-highlight-pen'/>")));
+					$('<div class="ktane-highlight-btn column">').data('mode', 'column').css({ right: 170 }).appendTo(document.body)
+						.append($(penSvg.replace(/\{0\}/g, "<path d='M 400 0 600 0 600 1000 400 1000 z' class='ktane-highlight-pen'/>")));
 				}
 			}
 
 			$('.ktane-highlight-btn').click(function()
 			{
-				$('.ktane-highlight-btn').removeClass('selected');
 				var newMode = $(this).data('mode');
-				if (newMode === mode)
+				if (newMode === 'color')
+					setColor((currentColor + 1) % colors.length);
+				else
 				{
-					mode = null;
-					return false;
+					$('.ktane-highlight-btn').removeClass('selected');
+					if (newMode === mode)
+						mode = null;
+					else
+					{
+						mode = newMode;
+						$(this).addClass('selected');
+					}
 				}
-
-				mode = newMode;
-				$(this).addClass('selected');
+				return false;
 			});
 
 			mobileControls = true;
 		}
+
+		setColor(0);
 
 		function getMode(event)
 		{
@@ -109,15 +130,15 @@ e.onload = function()
 			return ((ctrl && !shift) ? 'column' : (shift && !ctrl) ? 'row' : (shift && ctrl) ? 'element' : null);
 		}
 
-        function setPosition(highlight)
-        {
-            var a = highlight.data('obj-a'), b = highlight.data('obj-b');
-            highlight.outerWidth(a.outerWidth());
-            highlight.outerHeight(b.outerHeight());
-            highlight.css("left", a.offset().left + "px");
-            highlight.css("top", b.offset().top + "px");
-            highlight.css("transform-origin", -a.offset().left + "px " + -b.offset().top + "px");
-        }
+		function setPosition(highlight)
+		{
+			var a = highlight.data('obj-a'), b = highlight.data('obj-b');
+			highlight.outerWidth(a.outerWidth());
+			highlight.outerHeight(b.outerHeight());
+			highlight.css("left", a.offset().left + "px");
+			highlight.css("top", b.offset().top + "px");
+			highlight.css("transform-origin", -a.offset().left + "px " + -b.offset().top + "px");
+		}
 
 		$("td, th, li, .highlightable").each(function()
 		{
@@ -173,8 +194,8 @@ e.onload = function()
 						var svg = element.is("svg *");
 						var fill;
 
-						var highlight = $("<div>").addClass("ktane-highlight").data('obj-a', a).data('obj-b', b).css("background-color", currentColor);
-                        setPosition(highlight);
+						var highlight = $("<div>").addClass("ktane-highlight").data('obj-a', a).data('obj-b', b).css({ 'background-color': colors[currentColor], position: 'absolute' });
+						setPosition(highlight);
 
 						if (svg)
 						{
@@ -223,13 +244,13 @@ e.onload = function()
 			});
 		});
 
-        $(window).resize(function() 
-        {
-            $('.ktane-highlight').each(function(_, e) 
-            {
-                setPosition($(e));
-            });
-        });
+		$(window).resize(function()
+		{
+			$('.ktane-highlight').each(function(_, e)
+			{
+				setPosition($(e));
+			});
+		});
 	});
 };
 document.head.appendChild(e);
