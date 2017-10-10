c6595d8902d9fc5aeb24651e2c781fe178a1297d
Timwi <github@timwi.de>
2017-10-10 15:53:29 +0200

    Fix big in highlighter on non-mobile

--- HTML/js/highlighter.js
+++ HTML/js/highlighter.js
@@ -21,7 +21,7 @@ e.onload = function()
 		var colors = ["rgba(68, 130, 255, 0.4)", "rgba(223, 32, 32, 0.4)", "rgba(34, 195, 34, 0.4)", "rgba(223, 223, 32, 0.4)"];
 		var currentColor;
 		var setColor = function(color) {
-			currentColor = colors[color];
+			currentColor = color;
 		};
 
 		var highlighterAlert = $('<div style="position: fixed;top: 0;left: 0;padding: 6px;color: black;border-bottom:1px rgba(0, 0, 0, 0.5) solid;border-right:1px rgba(0, 0, 0, 0.5) solid;border-bottom-right-radius:5px;">Highlighter Color</div>').appendTo("body").hide();
