29a89f83061a1a83382fa8caf863d8602f7dbd7f
Timwi <github@timwi.de>
2018-08-28 09:43:47 +0200

    Rule seed support in Big Circle and Morse-A-Maze HTML manuals (via URL fragment; e.g. Big%20Circle.html#47 for rule seed 47)

---
+++ HTML/js/ruleseed.js
@@ -0,0 +1,121 @@
+class MonoRandom
+{
+    // Initializes a new instance of the class using the specified seed value.
+    constructor(seed)
+    {
+        this.seed = seed;
+        this._seedArray = new Array(56);
+        for (var i = 0; i < 56; i++)
+            this._seedArray[i] = 0;
+        var num = ((161803398 - (Math.abs(seed))) | 0);
+        this._seedArray[55] = num;
+        var num2 = 1;
+
+        for (var i = 1; i < 55; i = ((i + 1) | 0)) {
+          var num3 = ((Math.imul(21, i) % 55) | 0);
+          this._seedArray[num3] = num2;
+          num2 = ((num - num2) | 0);
+          if (num2 < 0) {
+            num2 = ((num2 + 2147483647) | 0);
+          }
+          num = (this._seedArray[num3] | 0);
+        }
+
+        for (var j = 1; j < 5; j = ((j + 1) | 0)) {
+
+          for (var k = 1; k < 56; k = ((k + 1) | 0)) {
+            this._seedArray[k] = (((this._seedArray[k] | 0) - (this._seedArray[((1 + ((((k + 30) | 0) % 55) | 0)) | 0)] | 0)) | 0);
+            if ((this._seedArray[k] | 0) < 0) {
+              this._seedArray[k] = (((this._seedArray[k] | 0) + 2147483647) | 0);
+            }
+          }
+        }
+        this._inext = 0;
+        this._inextp = 31;
+    }
+
+    // Returns a random number between 0.0 and 1.0.
+    nextDouble()
+    {
+        if (((++this._inext)|0) >= (56|0))
+            this._inext = 1|0;
+        if (((++this._inextp)|0) >= (56|0))
+            this._inextp = 1|0;
+        var num = ((this._seedArray[this._inext|0]|0) - (this._seedArray[this._inextp|0]|0))|0;
+        if ((num|0) < 0)
+            num = ((num|0) + 2147483647)|0;
+        this._seedArray[this._inext|0] = num|0;
+        return +(num * 4.6566128752457969E-10);
+    }
+
+    // Returns a non-negative random integer.
+    nextInt()
+    {
+        return ((+this.nextDouble() * 2147483647) | 0);
+    }
+
+    // Returns a non-negative random integer less than the specified maximum.
+    nextMax(maxValue)
+    {
+        return ((+this.nextDouble() * +maxValue) | 0);
+    }
+
+    // Returns a random integer within the specified range (minValue is inclusive, maxValue is exclusive).
+    next(minValue, maxValue)
+    {
+        if (maxValue - minValue <= 1)
+            return minValue;
+        return this.nextMax(maxValue - minValue) + minValue;
+    }
+
+    // Brings an array into random order.
+    shuffleArray(arr)
+    {
+        var sortArr = new Array(arr.length);
+        for (var i = 0; i < arr.length; i++)
+            sortArr[i] = { r: this.nextDouble(), v: arr[i] };
+        sortArr.sort((a, b) => a.r - b.r);
+        return sortArr.map(x => x.v);
+    }
+}
+
+var presumedModuleName = null;
+
+function ruleseedInvokeSetRules()
+{
+    if (presumedModuleName === null)
+        presumedModuleName = document.getElementsByClassName('page-header-section-title')[0].innerText;
+
+    if (/^#(\d+)$/.exec(window.location.hash) && (RegExp.$1 | 0) !== 1)
+    {
+        var seed = RegExp.$1 | 0;
+        document.body.classList.add('ruleseed-active');
+        Array.from(document.getElementsByClassName('ruleseed-header')).forEach(x => { x.innerText = 'RULE SEED: ' + seed});
+        Array.from(document.getElementsByClassName('page-header-section-title')).forEach(x => { x.classList.add('ruleseed-seeded'); x.innerText = presumedModuleName + ' — rule seed: ' + seed; });
+        setRules(new MonoRandom(seed));
+    }
+    else
+    {
+        document.body.classList.remove('ruleseed-active');
+        Array.from(document.getElementsByClassName('page-header-section-title')).forEach(x => { x.classList.remove('ruleseed-seeded'); x.innerText = presumedModuleName; });
+        setDefaultRules(new MonoRandom(1));
+    }
+}
+
+function ruleseedOnLoad()
+{
+    Array.from(document.getElementsByClassName('section')).forEach(x =>
+    {
+        var div = document.createElement('div');
+        div.className = 'ruleseed-header';
+        x.insertBefore(div, x.firstChild);
+
+        div = document.createElement('div');
+        div.className = 'ruleseed-header';
+        x.insertBefore(div, null);
+    });
+    ruleseedInvokeSetRules();
+}
+
+window.onload = ruleseedOnLoad;
+window.onhashchange = ruleseedInvokeSetRules;
