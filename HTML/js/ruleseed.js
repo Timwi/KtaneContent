8f5af819f146aae27b4841ba487f53677419ed32
Timwi <github@timwi.de>
2018-08-29 13:49:43 +0200

    Use DOMContentLoaded instead of onload

--- HTML/js/ruleseed.js
+++ HTML/js/ruleseed.js
@@ -11,24 +11,24 @@ class MonoRandom
         this._seedArray[55] = num;
         var num2 = 1;
 
-        for (var i = 1; i < 55; i = ((i + 1) | 0)) {
-          var num3 = ((Math.imul(21, i) % 55) | 0);
-          this._seedArray[num3] = num2;
-          num2 = ((num - num2) | 0);
-          if (num2 < 0) {
-            num2 = ((num2 + 2147483647) | 0);
-          }
-          num = (this._seedArray[num3] | 0);
+        for (var i = 1; i < 55; i = ((i + 1) | 0))
+        {
+            var num3 = ((Math.imul(21, i) % 55) | 0);
+            this._seedArray[num3] = num2;
+            num2 = ((num - num2) | 0);
+            if (num2 < 0)
+                num2 = ((num2 + 2147483647) | 0);
+            num = (this._seedArray[num3] | 0);
         }
 
-        for (var j = 1; j < 5; j = ((j + 1) | 0)) {
-
-          for (var k = 1; k < 56; k = ((k + 1) | 0)) {
-            this._seedArray[k] = (((this._seedArray[k] | 0) - (this._seedArray[((1 + ((((k + 30) | 0) % 55) | 0)) | 0)] | 0)) | 0);
-            if ((this._seedArray[k] | 0) < 0) {
-              this._seedArray[k] = (((this._seedArray[k] | 0) + 2147483647) | 0);
+        for (var j = 1; j < 5; j = ((j + 1) | 0))
+        {
+            for (var k = 1; k < 56; k = ((k + 1) | 0))
+            {
+                this._seedArray[k] = (((this._seedArray[k] | 0) - (this._seedArray[((1 + ((((k + 30) | 0) % 55) | 0)) | 0)] | 0)) | 0);
+                if ((this._seedArray[k] | 0) < 0)
+                    this._seedArray[k] = (((this._seedArray[k] | 0) + 2147483647) | 0);
             }
-          }
         }
         this._inext = 0;
         this._inextp = 31;
@@ -37,14 +37,14 @@ class MonoRandom
     // Returns a random number between 0.0 and 1.0.
     nextDouble()
     {
-        if (((++this._inext)|0) >= (56|0))
-            this._inext = 1|0;
-        if (((++this._inextp)|0) >= (56|0))
-            this._inextp = 1|0;
-        var num = ((this._seedArray[this._inext|0]|0) - (this._seedArray[this._inextp|0]|0))|0;
-        if ((num|0) < 0)
-            num = ((num|0) + 2147483647)|0;
-        this._seedArray[this._inext|0] = num|0;
+        if (((++this._inext) | 0) >= (56 | 0))
+            this._inext = 1 | 0;
+        if (((++this._inextp) | 0) >= (56 | 0))
+            this._inextp = 1 | 0;
+        var num = ((this._seedArray[this._inext | 0] | 0) - (this._seedArray[this._inextp | 0] | 0)) | 0;
+        if ((num | 0) < 0)
+            num = ((num | 0) + 2147483647) | 0;
+        this._seedArray[this._inext | 0] = num | 0;
         return +(num * 4.6566128752457969E-10);
     }
 
@@ -90,7 +90,7 @@ function ruleseedInvokeSetRules()
     {
         var seed = RegExp.$1 | 0;
         document.body.classList.add('ruleseed-active');
-        Array.from(document.getElementsByClassName('ruleseed-header')).forEach(x => { x.innerText = 'RULE SEED: ' + seed});
+        Array.from(document.getElementsByClassName('ruleseed-header')).forEach(x => { x.innerText = 'RULE SEED: ' + seed });
         Array.from(document.getElementsByClassName('page-header-section-title')).forEach(x => { x.classList.add('ruleseed-seeded'); x.innerText = presumedModuleName + ' — rule seed: ' + seed; });
         setRules(new MonoRandom(seed));
     }
@@ -102,7 +102,7 @@ function ruleseedInvokeSetRules()
     }
 }
 
-function ruleseedOnLoad()
+document.addEventListener("DOMContentLoaded", function()
 {
     Array.from(document.getElementsByClassName('section')).forEach(x =>
     {
@@ -115,7 +115,6 @@ function ruleseedOnLoad()
         x.insertBefore(div, null);
     });
     ruleseedInvokeSetRules();
-}
+});
 
-window.onload = ruleseedOnLoad;
 window.onhashchange = ruleseedInvokeSetRules;
