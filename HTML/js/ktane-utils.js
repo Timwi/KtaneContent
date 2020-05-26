d6d3be83662c39315d768067096e2c5fdd79bf7d
Andrew Smith <ChucklesTheBeard@users.noreply.github.com>
2020-05-26 15:06:10 -0400

    correct parenthesis
    ...started with a 2nd line of logic, realized there was a way to simplify it and didn't finish fixing the parenthesis. Whoops.

--- HTML/js/ktane-utils.js
+++ HTML/js/ktane-utils.js
@@ -86,7 +86,7 @@ e.onload = function()
         $(document).keydown(function(event)
         {
             // Only accept shortcuts with Alt or Ctrl+Shift
-            if ((!event.altKey && !(event.shiftKey && event.ctrlKey))
+            if (!event.altKey && !(event.shiftKey && event.ctrlKey))
                 return;
 
             // Alt-O: Open options menu
