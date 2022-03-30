9fc7eff7e45a4f1421a8f570d112b97445a93945
Luminoscity <luminoscity@gmail.com>
2022-03-30 00:29:28 -0700

    hexagonPath V and H

--- HTML/js/Utilities/svg-utils.js
+++ HTML/js/Utilities/svg-utils.js
@@ -5,10 +5,17 @@ function makeSvgElem(tag, attrs) {
     return el;
 }
 
-function hexagonPath(x, y, side) {
+function hexagonPathV(x, y, side) {
     let dx = Math.round(1000 * side * 0.8660254) / 1000.0;
     let dy = Math.round(1000 * side * 0.5) / 1000.0;
     let tx = Math.round(1000 * x) / 1000.0;
     let ty = Math.round(1000 * y) / 1000.0;
     return `M${tx} ${ty}l${dx}-${dy}l${dx} ${dy}v${side}l-${dx} ${dy}l-${dx}-${dy}z`;
+}
+function hexagonPathH(x, y, side) {
+    let dx = Math.round(1000 * side * 0.5) / 1000.0;
+    let dy = Math.round(1000 * side * 0.8660254) / 1000.0;
+    let tx = Math.round(1000 * x) / 1000.0;
+    let ty = Math.round(1000 * y) / 1000.0;
+    return `M${tx} ${ty}h${side}l${dx} ${dy}l-${dx} ${dy}h-${side}l-${dx}-${dy}z`;
 }
