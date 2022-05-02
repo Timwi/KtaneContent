c9ea39eff620ed427dc6f2cc0e4e4e09a3dce5f2
Luminoscity <luminoscity@gmail.com>
2022-05-02 05:23:34 -0700

    ui-utils

---
+++ HTML/js/Utilities/ui-utils.js
@@ -0,0 +1,36 @@
+/* Library for common user interface utilities. */
+
+/**
+ * Setup a function to repeatedly call when holding down a button or object.
+ * @param {Any} btn - Object to be held down with mouse cursor.
+ * @param {function} func - Functon to repeatedly call while holding the button.
+ * @param {int} startTime - Starting milliseconds, before acceleration, between repeats.
+ * @param {float} speedup - Rate of acceleration, divides the delay between repeats.
+ * @param {int} min - Minimum milliseconds between repeats to accelerate to.
+ */
+function WhenHoldDown(btn, func, startTime, speedup = 1.2, min = 10) {
+    let tm;
+    let s = startTime;
+    let repeat = function () {
+        func();
+        tm = setTimeout(repeat, s);
+        s = Math.max(s / speedup, min);
+    };
+    function clearTheRepeat() {
+        clearTimeout(tm);
+        s = startTime;
+    }
+
+    btn.onmousedown = function () { repeat(); };
+    btn.onmouseup = function () { clearTheRepeat(); };
+    btn.onmouseleave = function () { clearTheRepeat(); };
+}
+
+/**
+ * Requires jquery. Check that interaction event included none of shift, ctrl, alt, or cmd.
+ * @param {event} event - Event object from click or keydown function.
+ * @returns {Boolean} No special keys are pressed.
+ */
+function NoSpecialKeys(event) {
+    return !(event.shiftKey || event.ctrlKey || event.altKey || event.metaKey);
+}
