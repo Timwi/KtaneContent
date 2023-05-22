7ee100d2183f39cbcf01297ce1e9a2b11a4611b6
Timwi <github@timwi.de>
2023-05-22 19:29:19 +0200

    Upgrade jQuery and jQueryUI

--- puzzles/highlighter.js
+++ puzzles/highlighter.js
@@ -12,7 +12,7 @@
 
 var protocol = location.protocol;
 var e = document.createElement("script");
-e.src = "../../HTML/js/jquery.3.1.1.min.js";
+e.src = "../../HTML/js/jquery.3.7.0.min.js";
 e.onload = function()
 {
     $(function()
