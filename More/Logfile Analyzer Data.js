8ba3549fb8647c19dc191291c94adb3e548ee09b
Hawker <62062830+BlckHawker@users.noreply.github.com>
2023-07-29 06:21:29 -0700

    Finished Forget Us Not LFA (#1451)
    * Finished LFA
    
    * Make clearer when stages have not yet been shown.
    
    ---------
    
    Co-authored-by: TheKuroEver <mariusztang@outlook.com>

--- More/Logfile Analyzer Data.js
+++ More/Logfile Analyzer Data.js
@@ -5362,12 +5362,44 @@ let parseData = [
 		loggingTag: "Forget Us Not",
 		matches: [
 			{
+				regex: /Non-FUN modules: (\d+)/,
+				handler: function (matches, module) {
+					module.push(matches[0]);
+					const length = parseInt(matches[1]);
+					module.stageOrder = Array(length);
+					module.dropdowns = Array(length);
+					return true;
+				}
+			},
+			{
 				regex: /Stage order:(.+)/,
 				handler: function (matches, module) {
 					module.push({
 						label: "Stage order:",
 						obj: pre(matches[1])
 					});
+					for (let i = 0; i < module.dropdowns.length; i++) {
+						module.dropdowns[i] = [`Stage ${i + 1} = ?`, [ "This stage has not been shown yet." ]];
+						module.push(module.dropdowns[i]);
+					}
+					return true;
+				}
+			},
+			{
+				regex: /Stage = (\d+)/,
+				handler: function (matches, module) {
+					const index = parseInt(matches[1]) - 1;
+					const lines = readTaggedLines(3);
+					const dropdown = lines.slice(0, 2);
+					module.dropdowns[index][0] = lines[2];
+					module.dropdowns[index][1] = dropdown;
+					return true;
+				}
+			},
+			{
+				regex: /Solution = (\d+)/,
+				handler: function (matches, module) {
+					module.push("Solution = " + matches[1].match(/.{1,3}/g).join(" "))
 					return true;
 				}
 			},
