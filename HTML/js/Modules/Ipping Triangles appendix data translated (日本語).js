42466d8e76b6cb73f9225964fbfd7bbc954d641d
TheKuroEver <mariusztang@outlook.com>
2023-09-25 16:41:01 +0100

    Simplify and clarify ambiguity in Ipping Triangles appendix
    The ambiguity is between Tipping and Dripping triangles. It has been fixed by making the description of Dripping Triangles slightly more specific ('with at least one orange')

---
+++ HTML/js/Modules/Ipping Triangles appendix data translated (日本語).js
@@ -0,0 +1,59 @@
+const appendixHeader = "付録1PP1NG：オノマトペ三角形モジュール群の特定";
+const appendixEntries = [
+    {
+        name: "Tipping",
+        explanation: "すべての三角形が赤、黄、青のいずれかであり色が変化しない:",
+        translation: {
+            name: "ジャラジャラ三角形",
+            link: "Tipping Triangles translated (日本語 — ジャラジャラ三角形) (hatosable).html"
+        }
+    },
+    {
+        name: "Tripping",
+        explanation: "いずれかの三角形が二色に変化する:",
+        translation: {
+            name: "テクテク三角形",
+            link: "Tripping Triangles translated (日本語 — テクテク三角形) (hatosable).html"
+        }
+    },
+    {
+        name: "Flipping",
+        explanation: "すべての三角形が灰色または消灯している:",
+        translation: {
+            name: "パタパタ三角形",
+            link: "Flipping Triangles translated (日本語 — パタパタ三角形) (hatosable).html"
+        }
+    },
+    {
+        name: "Dripping",
+        explanation: "すべての三角形が赤、橙、黄のいずれかであり、少なくとも1つは橙である:",
+        translation: {
+            name: "ポタポタ三角形",
+            link: "Dripping Triangles translated (日本語 — ポタポタ三角形) (hatosable).html"
+        }
+    },
+    {
+        name: "Slipping",
+        explanation: "すべての三角形が緑、青、紫のいずれかである:",
+        translation: {
+            name: "ツルツル三角形",
+            link: "Slipping Triangles translated (日本語 — ツルツル三角形) (hatosable).html"
+        }
+    },
+    {
+        name: "Skipping",
+        explanation: "すべての三角形が橙、緑、紫のいずれかである:",
+        translation: {
+            name: "ピョンピョン三角形",
+            link: "Skipping Triangles translated (日本語 — ピョンピョン三角形) (hatosable).html"
+        }
+    },
+    {
+        name: "Clipping",
+        explanation: "ハイライトが複数の三角形にまたがっている:",
+        translation: {
+            name: "チョキチョキ三角形",
+            link: "Clipping Triangles translated (日本語 — チョキチョキ三角形) (hatosable).html"
+        }
+    }
+];
