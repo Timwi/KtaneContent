0111d4bc7c9398137b4f031024e8ac37c73961d1
Timwi <github@timwi.de>
2023-09-30 13:11:51 +0200

    Lion’s Share: move the ruleseed JS to a separate file; auto-adjust the legend so translators won’t have to do it manually
    (this also fixes an old bug in the black-and-white version)
    (also autoformat ruleseed.js)

---
+++ HTML/js/Modules/Lion's Share.js
@@ -0,0 +1,290 @@
+let maleNames = ["Abasi","Abdu","Adhama","Amani","Anasa","Ashon","Atieno","Ayo","Azima","Azizi","Badru","Baraka","Barasa","Bayana","Beno","Busar","Busara","Bwana","Chane","Daudi","Duma","Elea","Eli","Fanaka","Faraji","Hali","Hamadi","Hamidi","Hamisi","Hodari","Ikeno","Jafari","Jamba","Jata","Jela","Juma","Jumaane","Kamari","Kiama","Kifeda","Kiira","Kito","Kitwana","Kobe","Koman","Kuende","Maulidi","Mbita","Mhina","Mosi","Moyo","Mpenda","Mshindi","Msia","Murati","Musa","Mwinyi","Mzuzi","Neema","Njogwa","Nyo","Rajabu","Safiri","Salehe","Salene","Sefu","Shani","Shomari","Tabia","Taji","Tambo","Tamu","Tian","Umoja","Usian","Yahya","Zahur","Zakia","Zawadi","Abelo","Amahle","Amehlo","Andile","Anele","Anelisa","Avela","Ayanda","Ayize","Azisa","Bafana","Bandile","Bheka","Bhekifa","Bongani","Busiso","Cakaza","Cashile","Cebo","Cela","Chacha","Chaka","Chakide","Chipo","Chotho","Chweba","Ciko","Cilonga","Cothoza","Cwatha","Dabu","Dabuka","Daza","Delani","Dida","Dinfa","Dingane","Dingani","Donya","Dube","Duma","Dumisa","Dumo","Duna","Dundu","Eethaba","Elethu","Falakhe","Fanyana","Fikile","Fobela","Fokazi","Fowabo","Fowenu","Fudu","Fundani","Fundisa","Fuza","Fuzo","Gabadi","Gangi","Gania","Ganya","Gatsha","Gazi","Gazini","Gebhuza","Geza","Gibeli","Gijimi","Goduka","Guduza","Gugu","Gwala","Gwazi","Gwedi","Gwili","Hlelile","Ibubesi","Ifu","Igama","Ikhanda","Impela","Impi","Impisi","Indlovu","Induna","Ingane","Ingwe","Inyoni","Iqaqa","Iqhunde","Izagala","Izula","Jabu","Jama","Kgabu","Khwezi","Khulani","Kubu","Kufika","Kwanele","Kwazulu","Lethu","Lindani","Lindela","Lindile","Litha","Lizwi","Lunga","Lungelo","Lungile","Luvo","Luyanda","Lwazi","Makhaya","Malusi","Manqoba","Mapoza","Mbube","Mcebo","Mduduzi","Menzi","Mfundo","Mhambi","Mmeli","Mndeni","Mnotho","Mnqobi","Mondli","Mpilo","Mpucuko","Msizi","Mthunzi","Musa","Mvelo","Mzamo","Ndonsa","Nduduzo","Ndumiso","Nhlahla","Nikhil","Njabulo","Nkosi","Nqobani","Nsizwa","Owethu","Phakama","Phila","Philani","Qhude","Qiniso","Sakhile","Sandile","Sanele","Sbu","Sduduzo","Senzo","Sfiso","Sibonele","Sifiso","Sihle","Siphiwe","Sipho","Siyanda","Siza","Sizani","Sizwe","Tandie","Thabo","Thando","Thatha","Themba","Thulani","Ubaba","Ukuza","Ulwazi","Umzuzu","Unathi","Uyise","Vala","Velaphi","Vukani","Vusi","Vuyo","Wandile","Wenzile","Xhegu","Xhibeni","Xolani","Zamani","Zenzele","Zonke"];
+let femaleNames = ["Adhama","Adia","Afiya","Aishia","Akina","Aleela","Aluna","Amana","Amanika","Andaiye","Angalia","Arusi","Ashura","Asya","Atiena","Ayah","Ayubu","Bahati","Bakari","Baraka","Barika","Bavana","Budhya","Busara","Chagina","Chanua","Chiku","Chinira","Dafina","Dalia","Dalila","Dinka","Elea","Elewa","Endana","Endelea","Fanaka","Faraji","Fikira","Gethera","Goma","Halima","Halina","Halisi","Hasana","Hasina","Hawa","Heshima","Himaya","Hodari","Imani","Imara","Inira","Inithia","Itanya","Jaha","Jahaira","Jamaa","Jamani","Jamba","Jehlani","Jiona","Julisha","Kakena","Kalere","Kaluwa","Kamara","Kamaria","Kanene","Kanika","Kanisa","Karama","Kenura","Kesi","Khadija","Kiama","Kiania","Kibibi","Kichea","Kiira","Kilinda","Kinaya","Kinjia","Kito","Koffi","Kudio","Kuende","Kwashi","Lindana","Lindia","Lisha","Madini","Mahiri","Majda","Maji","Majida","Malika","Maliza","Marini","Mashika","Masika","Maulidi","Milima","Mkiwa","Msia","Mwamini","Mwasaa","Mwatabu","Nadira","Najuma","Nbushe","Neema","Nigesa","Nurisha","Nyota","Onyesha","Otesha","Oyana","Panya","Penda","Radhiya","Rasheda","Rashida","Rasida","Raziya","Rehema","Risala","Rukiya","Saada","Safika","Safiri","Salama","Sanura","Sauda","Shani","Shauri","Siti","Subira","Taabu","Tabara","Taji","Tamu","Tia","Tisa","Tuliza","Ujamaa","Ujana","Umija","Usia","Waseme","Winda","Zahra","Zaida","Zakiya","Zawadi","Zawati","Zuri","Zuwena","Amahle","Amehlo","Andile","Anele","Anelisa","Aphiwe","Ayanda","Ayize","Bheka","Bongani","Bongile","Bongiwe","Buhle","Cabanga","Cebile","Dade","Dansa","Davathi","Deliwe","Dida","Didiza","Dumile","Ejaj","Ekuseni","Elethu","Enama","Enanela","Esasa","Ethuka","Ethulo","Ethwasa","Fahlasi","Fakazi","Fikile","Fudu","Fula","Funani","Futhi","Gagasi","Gatsha","Gede","Gonothi","Gqamile","Gugu","Icici","Ifu","Impela","Impisi","Indlovu","Ingani","Ingwe","Inyoni","Ishumi","Izula","Jabhile","Jezile","Kgabu","Khanya","Khaya","Kholiwe","Khule","Khwezi","Kwanele","Lethiwe","Lethu","Lindiwe","Londiwe","Lungelo","Lungile","Luvo","Lwandle","Mbali","Mhambi","Minehle","Mlilo","Mondli","Msizi","Mthunzi","Naledi","Nandi","Ndonsa","Ndumiso","Njabulo","Njabulu","Nobantu","Nobuhle","Nobuntu","Nokwazi","Nolwazi","Nomcebo","Nompilo","Nomsa","Nomusa","Nomvula","Nomzamo","Nonhle","Nosipho","Noxolo","Nozipho","Nozizwe","Nqobile","Ntokozo","Ntombi","Ogogo","Philile","Qhikiza","Qiniso","Ramla","Sakhile","Sandile","Sibahle","Sifiso","Sihle","Siphiwe","Sipho","Siyanda","Siza","Sizani","Sizwe","Sonto","Thabile","Thabisa","Thandie","Thando","Themba","Thembi","Thoko","Thulani","Thulile","Uju","Ukudala","Ukuza","Umakoti","Umzuzu","Unathi","Unina","Usizo","Vala","Velile","Vemvane","Vumela","Vumile","Vuyiswa","Welile","Winile","Xolani","Xolile","Zamani","Zamile","Zandile","Zanele","Zibu","Zinhle","Zodwa","Zola","Zongile","Zonke","Zula"];
+let blackAndWhite = document.currentScript.getAttribute('blackandwhite') === 'true';
+
+function filterIndexes(list, predicate)
+{
+	let result = [];
+	for (let i = 0; i < list.length; i++)
+		if (predicate(list[i]))
+			result.push(i);
+	return result;
+}
+
+function indexOfPredicate(list, predicate)
+{
+	for (let i = 0; i < list.length; i++)
+		if (predicate(list[i]))
+			return i;
+	return -1;
+}
+
+function lastIndexOfPredicate(list, predicate)
+{
+	for (let i = list.length-1; i >= 0; i--)
+		if (predicate(list[i]))
+			return i;
+	return -1;
+}
+
+let LionStatus = {
+	Null:     0,
+	Unborn:   1,
+	Cub:      2,
+	Adult:    3,
+	Absent:   4,
+	King:     5,
+	Dead:     6,
+	Visiting: 7
+};
+
+let defaultRules = null;
+
+function getDefaultRules()
+{
+	// Remember the default rules for Rule Seed 1
+	if (defaultRules !== null)
+		return;
+	defaultRules = { rules: {}, ruleText: {}, rulePath: {} };
+	Array.from(document.getElementsByClassName('rule')).forEach(elem => { defaultRules.rules[elem.getAttribute('id')] = elem.innerText; });
+	Array.from(document.getElementsByClassName('rule-text')).forEach(elem => { defaultRules.ruleText[elem.getAttribute('id')] = { text: elem.textContent, color: elem.getAttribute('fill'), fontStyle: elem.getAttribute('font-style') }; });
+	Array.from(document.getElementsByClassName('rule-path')).forEach(elem => { defaultRules.rulePath[elem.getAttribute('id')] = elem.getAttribute('d'); });
+}
+
+function setDefaultRules()
+{
+	getDefaultRules();
+	Array.from(document.getElementsByClassName('rule')).forEach(elem => { elem.innerText = defaultRules.rules[elem.getAttribute('id')]; });
+	Array.from(document.getElementsByClassName('rule-text')).forEach(elem => { elem.textContent = defaultRules.ruleText[elem.getAttribute('id')].text; elem.setAttribute('fill', defaultRules.ruleText[elem.getAttribute('id')].color); elem.setAttribute('font-style', defaultRules.ruleText[elem.getAttribute('id')].fontStyle); });
+	Array.from(document.getElementsByClassName('rule-path')).forEach(elem => { elem.setAttribute('d', defaultRules.rulePath[elem.getAttribute('id')]); });
+}
+
+function setRules(rnd)
+{
+	getDefaultRules();
+
+	function logPride()
+	{
+		console.log(pride.map(lion => `${lion.name} (${lion.sex}) = ${lion.status.join('')} (${lion.birthyear}/${lion.mother})`));
+	}
+
+	let pride, visiting;
+	while (true)
+	{
+		// Pick 35 lion names
+		let allNames = rnd.shuffleFisherYates(maleNames.slice(0).concat(femaleNames.slice(0)));
+		let candidate = 0;
+		let lions = [];
+		while (lions.length < 35)
+		{
+			let name = allNames[candidate];
+			candidate++;
+			if (lions.indexOf(name) === -1)
+				lions.push(name);
+		}
+
+		// Decide which lions are in the pride and which ones are visiting
+		pride = [];
+		visiting = [];
+		for (let i = 0; i < lions.length; i++)
+			(i < 18 ? pride : visiting).push({ name: lions[i], sex: maleNames.indexOf(lions[i]) === -1 ? 'f' : femaleNames.indexOf(lions[i]) === -1 ? 'm' : 'mf'.substr(rnd.next(0, 2), 1) });
+
+		// Decide on a random birthyear and lifespan for each lion (lifespan includes “unborn” phase)
+		for (let i = 0; i < pride.length; i++)
+		{
+			let lifespan = rnd.next(2, 15);
+			let cubhood = [1, 2, 2, 2, 2, 2, 3][rnd.next(0, 6)];
+			pride[i].birthyear = rnd.next(Math.max(-5, 2-lifespan), 17);
+			pride[i].status = new Array(16);
+			for (let year = 1; year <= 16; year++)
+				pride[i].status[year-1] =
+					year < pride[i].birthyear ? LionStatus.Null :
+					year === pride[i].birthyear ? LionStatus.Unborn :
+					year < pride[i].birthyear + cubhood + 1 ? LionStatus.Cub :
+					year < pride[i].birthyear + lifespan ? (rnd.next(0, 8) === 0 ? LionStatus.Absent : LionStatus.Adult) :
+					LionStatus.Dead;
+		}
+
+		// Make sure there’s an adult male and an adult female in every year so we can always have a king and a lead huntress
+		let valid = true;
+		let currentKing = null;
+		for (let year = 0; (year < 16) && valid; year++)
+		{
+			let potentialKings = filterIndexes(pride, lion => lion.sex === 'm' && lion.status[year] === LionStatus.Adult);
+			let potentialMothers = filterIndexes(pride, lion => lion.sex === 'f' && lion.status[year] === LionStatus.Adult);
+			if (potentialKings.length === 0 || potentialMothers.length === 0)
+				valid = false;
+			else
+			{
+				// Assign a king
+				if (currentKing === null || potentialKings.indexOf(currentKing) === -1)
+					currentKing = potentialKings[rnd.next(0, potentialKings.length)];
+				pride[currentKing].status[year] = LionStatus.King;
+
+				// Give greater weight to younger mothers
+				potentialMothers.sort((a, b) => {
+					let val = pride[b].birthyear - pride[a].birthyear;
+					if (val !== 0)
+						return val;
+					return pride[a].name < pride[b].name ? -1 : 1;
+				});
+				let chooseFrom = [];
+				for (let i = 0; i < potentialMothers.length; i++)
+					for (let j = 0; j <= i; j++)
+						chooseFrom.push(potentialMothers[j]);
+
+				// Give every unborn cub a mother
+				for (let i = 0; i < pride.length; i++)
+					if (pride[i].status[year] === LionStatus.Unborn)
+						pride[i].mother = pride[chooseFrom[rnd.next(0, chooseFrom.length)]].name;
+			}
+		}
+		if (!valid)
+			continue;
+
+		// Sort by birthyear and then make sure that the lifespans won’t overlap on the chart
+		valid = true;
+		pride.sort((a, b) => {
+			let val = a.birthyear - b.birthyear;
+			if (val !== 0)
+				return val;
+			return a.name < b.name ? -1 : 1;
+		});
+		for (let i = 0; (i < 6) && valid; i++)
+			if (pride[i].status.indexOf(LionStatus.Dead) === -1 || pride[i + 12].status [ pride[i].status.indexOf(LionStatus.Dead) ] !== LionStatus.Null)
+				valid = false;
+		if (!valid)
+			continue;
+
+		break;
+	}
+
+	// Lion names
+	for (let i = 0; i < 18; i++)
+	{
+		document.getElementById(`lion-${i}`).textContent = pride[i].name;
+		if (blackAndWhite)
+			document.getElementById(`lion-${i}`).setAttribute('font-style', pride[i].sex === 'm' ? 'italic': 'normal');
+		else
+			document.getElementById(`lion-${i}`).setAttribute('fill', pride[i].sex === 'm' ? '#347fcd': '#cd34c9');
+	}
+
+	// Colored bars showing the lions’ ages (does not include the boxes in the legend)
+	let bars = { unborn: '', cub: '', adult: '', absent: '', king: '' };
+	for (let i = 0; i < 18; i++)
+	{
+		pride[i].y = 15 + 10*(i < 6 ? 5-i : 17-i);
+		let year = indexOfPredicate(pride[i].status, status => status !== LionStatus.Null);
+		while (year < 16)
+		{
+			let status = pride[i].status[year];
+			let origYear = year;
+			while (year < 16 && pride[i].status[year] === status)
+				year++;
+			let length = year - origYear;
+			let bar = [null, 'unborn', 'cub', 'adult', 'absent', 'king', null, null][status];
+			if (bar !== null)
+				bars[bar] += `M${35 + 10*origYear},${pride[i].y}h${10*length}v10h${-10*length}z`;
+		}
+	}
+	for (let key in bars)
+		document.getElementById(`bar-${key}`).setAttribute('d', bars[key]);
+
+	// Dividing lines
+	let lines = '';
+	for (let y = 0; y < 13; y++)
+	{
+		let rightLionIxs = y == 0 ? [17] : y === 12 ? [6] : [17-y, 18-y];
+		let rx1 = 35 + 10*Math.min(...rightLionIxs.map(ix => indexOfPredicate(pride[ix].status, s => s !== LionStatus.Null)));
+		let rx2 = 225;
+		let my = 15 + 10*y;
+
+		if (y < 7)
+		{
+			let leftLionIxs = y == 0 ? [5] : y === 6 ? [0] : [6-y, 5-y];
+			let lx1 = 5;
+			let lx2 = 35 + 10*Math.max(...leftLionIxs.map(ix => lastIndexOfPredicate(pride[ix].status, s => s !== LionStatus.Dead) + 1));
+
+			if (lx2 >= rx1 || y === 0)
+			{
+				lines += `M${lx1},${my}H${rx2}`;
+				continue;
+			}
+			else
+				lines += `M${lx1},${my}H${lx2}`;
+		}
+		lines += `M${rx1},${my}H${rx2}`;
+	}
+	document.getElementById('dividers').setAttribute('d', lines);
+
+	// Lines showing unborn cubs’ mothers
+	lines = 'M5 110v15';
+	for (let year = 0; year < 16; year++)
+	{
+		let unbornCubs = pride.filter(lion => lion.status[year] === LionStatus.Unborn);
+		let w = 10 / (unbornCubs.length + 1);
+		for (let i = 0; i < unbornCubs.length; i++)
+			lines += `M${35 + 10*year + w*(i+1)},${unbornCubs[i].y + 5}V${pride[indexOfPredicate(pride, lion => lion.name === unbornCubs[i].mother)].y + 5}`;
+	}
+	document.getElementById('mothers').setAttribute('d', lines);
+
+	// Visiting lions
+	for (let i = 0; i < 17; i++)
+	{
+		document.getElementById(`visiting-${i}`).textContent = visiting[i].name;
+		if (blackAndWhite)
+			document.getElementById(`visiting-${i}`).setAttribute('font-style', visiting[i].sex === 'm' ? 'italic': 'normal');
+		else
+			document.getElementById(`visiting-${i}`).setAttribute('fill', visiting[i].sex === 'm' ? '#347fcd': '#cd34c9');
+	}
+
+	let kingPortion = rnd.next(10, 20);                 // 10–19
+	let adultSibling = kingPortion - rnd.next(1, 4);    // 7–18
+	let adult = adultSibling - rnd.next(1, 3);          // 5–17
+	let cubSibling = adult - rnd.next(1, 4);            // 2–16
+	let cub = Math.max(1, cubSibling - rnd.next(1, 4)); // 1–15
+	function unit(n) { return n === 1 ? '1 unit' : `${n} units`; }
+
+	document.getElementById('entitlement-king').innerText = unit(kingPortion);
+	document.getElementById('entitlement-adult-sibling').innerText = unit(adultSibling);
+	document.getElementById('entitlement-adult').innerText = unit(adult);
+	document.getElementById('entitlement-cub-sibling').innerText = unit(cubSibling);
+	document.getElementById('entitlement-cub').innerText = unit(cub);
+	document.getElementById('indicator').innerText = ['each', 'each lit', 'each unlit'][rnd.next(0, 3)];
+
+	let indKing = rnd.next(3, 6);               // 3–5
+	let indSibling = indKing - rnd.next(1, 3);  // 1–4
+	let indMale = rnd.next(1, 3);
+	let indFemale = 3 - indMale;
+	let sn = rnd.next(1, 3);
+
+	document.getElementById('entitlement-ind-king').innerText = unit(indKing);
+	document.getElementById('entitlement-ind-sibling').innerText = unit(indSibling);
+	document.getElementById('entitlement-ind-male').innerText = unit(indMale);
+	document.getElementById('entitlement-ind-female').innerText = unit(indFemale);
+	document.getElementById('entitlement-sn').innerText = unit(sn);
+
+	document.getElementById('lead-huntress-color').innerText = ['red', 'brown', 'yellow', 'green', 'turquoise', 'dark blue', 'purple', 'pink'][rnd.next(0, 8)];
+}
+
+window.addEventListener('load', function()
+{
+	// Rearrange the legend in case the manual is translated
+	let legends = [ 'unborn', 'cub', 'adult', 'king', 'absent' ].map(lg => document.getElementById(`legend-${lg}`));
+
+	let widths = legends.map(lg => lg.getBBox().width);
+	let totalWidth = 0;
+	for (let w of widths)
+		totalWidth += w;
+	console.log(totalWidth);
+	let availableSpace = 225 - totalWidth;
+	let spacing = availableSpace/8;
+	for (let lgIx = 0; lgIx < legends.length; lgIx++)
+	{
+		let lg = legends[lgIx];
+		let desiredX = (2 + lgIx) * spacing;
+		for (let lgIx2 = 0; lgIx2 < lgIx; lgIx2++)
+			desiredX += widths[lgIx2];
+		lg.setAttribute('transform', `translate(${desiredX - lg.getBBox().x}, 0)`);
+	}
+});
--- HTML/js/ruleseed.js
+++ HTML/js/ruleseed.js
@@ -1,110 +1,110 @@
 class MonoRandom
 {
-    // Initializes a new instance of the class using the specified seed value.
-    constructor(seed)
-    {
-        this.seed = seed;
-        this.count = 0;
-        this._seedArray = new Array(56);
-        for (var i = 0; i < 56; i++)
-            this._seedArray[i] = 0;
-        var num = ((161803398 - (Math.abs(seed))) | 0);
-        this._seedArray[55] = num;
-        var num2 = 1;
+	// Initializes a new instance of the class using the specified seed value.
+	constructor(seed)
+	{
+		this.seed = seed;
+		this.count = 0;
+		this._seedArray = new Array(56);
+		for (var i = 0; i < 56; i++)
+			this._seedArray[i] = 0;
+		var num = ((161803398 - (Math.abs(seed))) | 0);
+		this._seedArray[55] = num;
+		var num2 = 1;
 
-        for (var i = 1; i < 55; i = ((i + 1) | 0))
-        {
-            var num3 = ((Math.imul(21, i) % 55) | 0);
-            this._seedArray[num3] = num2;
-            num2 = ((num - num2) | 0);
-            if (num2 < 0)
-                num2 = ((num2 + 2147483647) | 0);
-            num = (this._seedArray[num3] | 0);
-        }
+		for (var i = 1; i < 55; i = ((i + 1) | 0))
+		{
+			var num3 = ((Math.imul(21, i) % 55) | 0);
+			this._seedArray[num3] = num2;
+			num2 = ((num - num2) | 0);
+			if (num2 < 0)
+				num2 = ((num2 + 2147483647) | 0);
+			num = (this._seedArray[num3] | 0);
+		}
 
-        for (var j = 1; j < 5; j = ((j + 1) | 0))
-        {
-            for (var k = 1; k < 56; k = ((k + 1) | 0))
-            {
-                this._seedArray[k] = (((this._seedArray[k] | 0) - (this._seedArray[((1 + ((((k + 30) | 0) % 55) | 0)) | 0)] | 0)) | 0);
-                if ((this._seedArray[k] | 0) < 0)
-                    this._seedArray[k] = (((this._seedArray[k] | 0) + 2147483647) | 0);
-            }
-        }
-        this._inext = 0;
-        this._inextp = 31;
-    }
+		for (var j = 1; j < 5; j = ((j + 1) | 0))
+		{
+			for (var k = 1; k < 56; k = ((k + 1) | 0))
+			{
+				this._seedArray[k] = (((this._seedArray[k] | 0) - (this._seedArray[((1 + ((((k + 30) | 0) % 55) | 0)) | 0)] | 0)) | 0);
+				if ((this._seedArray[k] | 0) < 0)
+					this._seedArray[k] = (((this._seedArray[k] | 0) + 2147483647) | 0);
+			}
+		}
+		this._inext = 0;
+		this._inextp = 31;
+	}
 
-    // Returns a random number between 0.0 and 1.0.
-    nextDouble(logging)
-    {
-        this.count++;
-        if (((++this._inext) | 0) >= (56 | 0))
-            this._inext = 1 | 0;
-        if (((++this._inextp) | 0) >= (56 | 0))
-            this._inextp = 1 | 0;
-        var num = ((this._seedArray[this._inext | 0] | 0) - (this._seedArray[this._inextp | 0] | 0)) | 0;
-        if ((num | 0) < 0)
-            num = ((num | 0) + 2147483647) | 0;
-        this._seedArray[this._inext | 0] = num | 0;
-        var result = +(num * 4.6566128752457969E-10);
-        if (logging)
-            console.log(`rnd.nextDouble() = ${result}`);
-        return result;
-    }
+	// Returns a random number between 0.0 and 1.0.
+	nextDouble(logging)
+	{
+		this.count++;
+		if (((++this._inext) | 0) >= (56 | 0))
+			this._inext = 1 | 0;
+		if (((++this._inextp) | 0) >= (56 | 0))
+			this._inextp = 1 | 0;
+		var num = ((this._seedArray[this._inext | 0] | 0) - (this._seedArray[this._inextp | 0] | 0)) | 0;
+		if ((num | 0) < 0)
+			num = ((num | 0) + 2147483647) | 0;
+		this._seedArray[this._inext | 0] = num | 0;
+		var result = +(num * 4.6566128752457969E-10);
+		if (logging)
+			console.log(`rnd.nextDouble() = ${result}`);
+		return result;
+	}
 
-    // Returns a non-negative random integer.
-    nextInt()
-    {
-        return ((+this.nextDouble() * 2147483647) | 0);
-    }
+	// Returns a non-negative random integer.
+	nextInt()
+	{
+		return ((+this.nextDouble() * 2147483647) | 0);
+	}
 
-    // Returns a non-negative random integer less than the specified maximum.
-    nextMax(maxValue)
-    {
-        return ((+this.nextDouble() * +maxValue) | 0);
-    }
+	// Returns a non-negative random integer less than the specified maximum.
+	nextMax(maxValue)
+	{
+		return ((+this.nextDouble() * +maxValue) | 0);
+	}
 
-    // Returns a random integer within the specified range (minValue is inclusive, maxValue is exclusive).
-    next(minValue, maxValue, logging)
-    {
-        var result;
-        if (maxValue - minValue <= 1)
-            result = minValue;
-        else
-            result = this.nextMax(maxValue - minValue) + minValue;
-        if (logging)
-            console.log(`rnd.next(${minValue}, ${maxValue}) = ${result}`);
-        return result;
-    }
+	// Returns a random integer within the specified range (minValue is inclusive, maxValue is exclusive).
+	next(minValue, maxValue, logging)
+	{
+		var result;
+		if (maxValue - minValue <= 1)
+			result = minValue;
+		else
+			result = this.nextMax(maxValue - minValue) + minValue;
+		if (logging)
+			console.log(`rnd.next(${minValue}, ${maxValue}) = ${result}`);
+		return result;
+	}
 
-    // Brings an array into random order.
-    // This method is equivalent to doing .OrderBy(x => rnd.NextDouble()) in C#.
-    // Returns a new array and leaves the original array unmodified.
-    shuffleArray(arr)
-    {
-        var sortArr = new Array(arr.length);
-        for (var i = 0; i < arr.length; i++)
-            sortArr[i] = { r: this.nextDouble(), v: arr[i] };
-        sortArr.sort((a, b) => a.r - b.r);
-        return sortArr.map(x => x.v);
-    }
+	// Brings an array into random order.
+	// This method is equivalent to doing .OrderBy(x => rnd.NextDouble()) in C#.
+	// Returns a new array and leaves the original array unmodified.
+	shuffleArray(arr)
+	{
+		var sortArr = new Array(arr.length);
+		for (var i = 0; i < arr.length; i++)
+			sortArr[i] = { r: this.nextDouble(), v: arr[i] };
+		sortArr.sort((a, b) => a.r - b.r);
+		return sortArr.map(x => x.v);
+	}
 
-    // Brings an array into random order using the Fisher-Yates shuffle.
-    // This is an inplace algorithm, i.e. the input array is modified.
-    shuffleFisherYates(list)
-    {
-        var i = list.length;
-        while (i > 1)
-        {
-            var index = this.next(0, i);
-            i--;
-            var value = list[index];
-            list[index] = list[i];
-            list[i] = value;
-        }
-        return list;
-    }
+	// Brings an array into random order using the Fisher-Yates shuffle.
+	// This is an inplace algorithm, i.e. the input array is modified.
+	shuffleFisherYates(list)
+	{
+		var i = list.length;
+		while (i > 1)
+		{
+			var index = this.next(0, i);
+			i--;
+			var value = list[index];
+			list[index] = list[i];
+			list[i] = value;
+		}
+		return list;
+	}
 }
 
 const names = {};
@@ -113,54 +113,58 @@ function ruleseedInvokeSetRules()
 {
 	let sectionElements = Array.from(document.getElementsByClassName('page-header-section-title'));
 	// Checking if the names object has a length of 0 so that we don't initialize it twice.
-	if(Object.entries(names).length === 0) {
+	if (Object.entries(names).length === 0)
+	{
 		let i = 0;
 		// Stores the headers by the id indency.
-		sectionElements.forEach(x => {
+		sectionElements.forEach(x =>
+		{
 			names[i] = x.innerText;
 			x.setAttribute('name-id', i);
 			i++;
 		});
 	}
-	
+
 	// Whether the URL contains a seed.
-    if (/^#(\d+)$/.exec(window.location.hash) && (RegExp.$1 | 0) !== 1)
-    {
-        var seed = RegExp.$1 | 0;
-        document.body.classList.add('ruleseed-active');
-        Array.from(document.getElementsByClassName('ruleseed-header')).forEach(x => { x.innerText = 'RULE SEED: ' + seed });
-		
+	if (/^#(\d+)$/.exec(window.location.hash) && (RegExp.$1 | 0) !== 1)
+	{
+		var seed = RegExp.$1 | 0;
+		document.body.classList.add('ruleseed-active');
+		Array.from(document.getElementsByClassName('ruleseed-header')).forEach(x => { x.innerText = 'RULE SEED: ' + seed; });
+
 		// Iterate over each page header section title element, add the 'ruleseed-seeded' class, get the actual section title by using the custom 'name-id' attribute and appending the rule seed value.
-        sectionElements.forEach(x => { 
-			x.classList.add('ruleseed-seeded'); 
-			x.innerText = names[x.getAttribute('name-id')] + ' — rule seed: ' + seed; 
+		sectionElements.forEach(x =>
+		{
+			x.classList.add('ruleseed-seeded');
+			x.innerText = names[x.getAttribute('name-id')] + ' — rule seed: ' + seed;
 		});
-        setRules(new MonoRandom(seed));
-    }
-    else
-    {
-        document.body.classList.remove('ruleseed-active');
-        sectionElements.forEach(x => { 
-			x.classList.remove('ruleseed-seeded'); 
+		setRules(new MonoRandom(seed));
+	}
+	else
+	{
+		document.body.classList.remove('ruleseed-active');
+		sectionElements.forEach(x =>
+		{
+			x.classList.remove('ruleseed-seeded');
 			x.innerText = names[x.getAttribute('name-id')];
 		});
-        setDefaultRules(new MonoRandom(1));
-    }
+		setDefaultRules(new MonoRandom(1));
+	}
 }
 
 document.addEventListener("DOMContentLoaded", function()
 {
-    Array.from(document.getElementsByClassName('section')).forEach(x =>
-    {
-        var div = document.createElement('div');
-        div.className = 'ruleseed-header';
-        x.insertBefore(div, x.firstChild);
+	Array.from(document.getElementsByClassName('section')).forEach(x =>
+	{
+		var div = document.createElement('div');
+		div.className = 'ruleseed-header';
+		x.insertBefore(div, x.firstChild);
 
-        div = document.createElement('div');
-        div.className = 'ruleseed-header';
-        x.insertBefore(div, null);
-    });
-    ruleseedInvokeSetRules();
+		div = document.createElement('div');
+		div.className = 'ruleseed-header';
+		x.insertBefore(div, null);
+	});
+	ruleseedInvokeSetRules();
 });
 
 window.onhashchange = ruleseedInvokeSetRules;
