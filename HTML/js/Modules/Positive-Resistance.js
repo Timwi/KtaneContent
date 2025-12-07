6ed99af7614979be5318c97f4a333fe47004b04d
UnrulyJuli3 <sid3rdark@gmail.com>
2025-12-07 12:45:40 -0800

    clean up positive-resistance logic

--- HTML/js/Modules/Positive-Resistance.js
+++ HTML/js/Modules/Positive-Resistance.js
@@ -1,26 +1,20 @@
-        let face = document.getElementById("face");
-        let presses = 0;
-        updateVisuals();
+const diagram = document.querySelector(".diagram"),
+    flavourText = document.querySelector(".flavour-text"),
+    content = document.querySelector(".page-content");
 
-        face.onclick = () => {
-            presses++;
-            updateVisuals();
-        };
-        function updateVisuals() {
-            if (presses < 6){
-                document.getElementById("proper").hidden = true;
-                document.getElementById("init").hidden = false;
-            }
-            else if (presses < 15){
-                document.getElementById("flavour-text").innerHTML = "Stop! What are you doing?!";
-            }
-            else if (presses < 30){
-                document.getElementById("flavour-text").innerHTML = "<strong>STOP! PLEASE!</strong>";
-            }
-            else{
-                document.getElementById("flavour-text").innerHTML = "What have you done?";
-                document.getElementById("proper").hidden = false;
-                document.getElementById("init").hidden = true;
-            }
-            //document.getElementById("flavour-text").innerHTML = `${presses == 0 ? "" : ` You pressed SVG ${presses} time${presses == 1 ? "" : "s"}.`}`;
-        }
+let presses = 0;
+
+const getFlavourText = () => {
+    switch (presses) {
+        case 6: return "Stop! What are you doing?!";
+        case 15: return "<strong>STOP! PLEASE!</strong>";
+        case 30: return "What have you done?";
+    }
+};
+
+diagram.addEventListener("click", () => {
+    presses++;
+    const text = getFlavourText();
+    if (text) flavourText.innerHTML = text;
+    if (presses === 30) content.classList.add("reveal");
+});
