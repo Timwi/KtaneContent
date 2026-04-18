b535fc9d4120315bf032c376fd789ae62d4e89b7
Rafa Teivfik <teivfik@gmail.com>
2026-04-18 15:04:25 -0300

    Freeform drawing: Added mobile controls

--- HTML/js/ktane-utils.js
+++ HTML/js/ktane-utils.js
@@ -270,6 +270,12 @@ if (!new URLSearchParams(window.location.search).has("merger")) {
             // Highlighter mobile controls
             let mode = null;
             let mobileControls = false;
+            let btnRightPos = 5;
+            const nextIconPos = () => {
+                const current = btnRightPos;
+                btnRightPos += 55;
+                return current;
+            }
             if ((/mobi/i.test(navigator.userAgent) || window.location.hash.indexOf('highlight') !== -1) && window.location.hash.indexOf('nohighlight') === -1)
             {
                 let styleTag = $("<style>").appendTo($("head"));
@@ -279,56 +285,98 @@ if (!new URLSearchParams(window.location.search).has("merger")) {
                     styleTag.text((
                         ".ktane-highlight-svg { width: 50px; height: 50px; }" +
                         ".ktane-highlight-pen { fill: !0!; }" +
-                        ".ktane-highlight-btn { position: fixed; top: 5px; width: 50px; height: 50px; border: 3px solid transparent; border-radius: 10px; }" +
+                        ".ktane-highlight-btn { position: fixed; top: 5px; width: 50px; height: 50px; border: 3px solid transparent; border-radius: 10px; z-Index: 10000; }" +
                         ".ktane-highlight-btn.selected { border-color: #4c6; }" +
                         "@media print { .ktane-highlight-btn { display: none; }  }")
                         .replace(/!0!/g, colors[color].color));
                 };
 
-                $('<div class="ktane-highlight-btn theme">').data("mode", "theme").css({ right: 5 }).appendTo(document.body).append(
-                    $("<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' class='ktane-highlight-svg'><path fill='none' d='M0 0h24v24H0z'/><path style='stroke:#000;stroke-linecap:round;stroke-linejoin:round;fill:#fff;fill-opacity:.5' d='M12 3a9 9 0 1 0 9 9c0-.46-.04-.92-.1-1.36a5.389 5.389 0 0 1-4.4 2.26 5.403 5.403 0 0 1-3.14-9.8c-.44-.06-.9-.1-1.36-.1h0z'/></svg>")
-                );
+                $('<div class="ktane-highlight-btn theme">')
+                    .data("mode", "theme")
+                    .css({ right: nextIconPos() })
+                    .appendTo(document.body)
+                    .append(
+                        $("<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' class='ktane-highlight-svg'><path fill='none' d='M0 0h24v24H0z'/><path style='stroke:#000;stroke-linecap:round;stroke-linejoin:round;fill:#fff;fill-opacity:.5' d='M12 3a9 9 0 1 0 9 9c0-.46-.04-.92-.1-1.36a5.389 5.389 0 0 1-4.4 2.26 5.403 5.403 0 0 1-3.14-9.8c-.44-.06-.9-.1-1.36-.1h0z'/></svg>")
+                    );
 
                 let penSvg = "<svg xmlns='http://www.w3.org/2000/svg' viewBox='-50 -50 1100 1100' class='ktane-highlight-svg'>{0}<path d='M816.3 32.7c-9.6 0-14.8 1-21 4.2C791 39.3 659 153 501.7 289.6c-247.8 215.5-291.3 254-328 291-56.7 57.4-55.6 53.8-37 115.6 5 17 6.3 24.5 6.3 38.3.3 33.5-6 43-74.4 111.6-29.7 30-54.8 56.2-56 58.5-1.4 2.3-2.4 5.7-2.6 7.6 0 5.2 5.4 14.4 10 17 9 4.6 125.4 38 133.4 38 8.3 0 8.7-.4 43-34 36.5-35.7 47-43.6 67.2-49.3 17.8-5.2 43.6-3.3 76.8 5.7 29.3 8 42.3 8.2 54.7 2 3.8-2 24.3-20.8 45.7-42 32.6-32.2 79.7-85.2 288-324.8C865.6 367.4 980 234.7 983 230c7.4-11.2 9-25 4.7-39.7-3.3-11-4.8-12.8-71.5-79.8-76.2-76.5-78-78-100-78zm1 38.3c2.2 0 27 23.2 68.5 64.6 57.5 57.4 64.5 65.2 64.2 70-.4 4-60.2 74.2-239 280L472.4 760.3l-105-104.8C310 598 263 550.2 263.3 549.5c.4-.7 124.3-108.5 275.4-240C704.6 165.6 815 71 817.2 71zM233 576.6L339.5 683 445.7 789l-33.5 33.5c-26.8 26.8-34.5 33.5-38.7 33.5-3 0-17.3-3-32-6.5-14.6-3.7-32.8-7.3-40-8-27-2.5-62.7 6-83.5 20-5 3.3-9.6 6-10.4 6-.7 0-13.2-12-27.7-26.4l-26.4-26.5 5-6.5c8.3-11 17.8-31.8 21-45.5 5.3-21.3 3-49-6.6-81.8-4.2-14.8-8-29-8-31.6 0-3.7 8.2-13 34-38.7l34-34zM127 844.6c.6 0 12.5 11.6 26.5 25.6l25.2 25.3-15.7 15.7-16 15.7-39.7-11.8-39.8-12L96.7 874c16-16 29.6-29.3 30.2-29.3z' fill='black'/><path d='M885.8 135.6c57.5 57.4 64.5 65.3 64.2 70-.4 4-60.2 74.2-239 280L472.4 760.3l-105-104.8C310 598 263 550.2 263.3 549.5c.4-.7 124.3-108.5 275.4-240C704.6 165.6 815 71 817.2 71c2.3 0 27 23.2 68.6 64.5zm-473.6 687c-26.8 27-34.5 33.6-38.7 33.6-3 0-17.2-3-32-6.5-14.6-3.6-32.7-7.3-40-7.8-27-2.8-62.6 5.6-83.5 19.6-5 3.4-9.6 6-10.4 6-.7 0-13.2-11.8-27.7-26.3L153.5 815l5-6.7c8.2-11 17.8-31.7 21-45.5 5.2-21.3 3-49-6.6-81.8-4.2-14.8-8-29-8-31.6 0-3.7 8.2-12.8 34-38.7l34-34 106.4 106.2L445.7 789l-33.5 33.5zm-259 47.6l25.3 25.3-15.7 15.7-16 15.7-39.7-11.8-39.7-12L96.6 874c16-16 29.7-29.3 30.2-29.3.6 0 12.5 11.5 26.5 25.5z' class='ktane-highlight-pen'/></svg>";
 
                 if ($('td, th, li, .highlightable').length)
                 {
-                    $('<div class="ktane-highlight-btn color">').data('mode', 'color').css({ right: 60 }).appendTo(document.body).append($((
-                        "<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 10.5 10.5' class='ktane-highlight-svg'>" +
-                        "<rect x='0.5' y='0.5' width='4.5' height='4.5' fill='!0!' rx='1' ry='1' />" +
-                        "<rect x='5.5' y='0.5' width='4.5' height='4.5' fill='!1!' rx='1' ry='1' />" +
-                        "<rect x='0.5' y='5.5' width='4.5' height='4.5' fill='!2!' rx='1' ry='1' />" +
-                        "<rect x='5.5' y='5.5' width='4.5' height='4.5' fill='!3!' rx='1' ry='1' />" +
-                        "</svg>").replace(/!(\d+)!/g, function(_, m) { return colors[+m].color; })));
-                    $('<div class="ktane-highlight-btn element">').data('mode', 'element').css({ right: 115 }).appendTo(document.body).append($(penSvg.replace(/\{0\}/g, '')));
+                    $('<div class="ktane-highlight-btn color">')
+                        .data('mode', 'color')
+                        .css({ right: nextIconPos() })
+                        .appendTo(document.body)
+                        .append($((
+                            "<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 10.5 10.5' class='ktane-highlight-svg'>" +
+                            "<rect x='0.5' y='0.5' width='4.5' height='4.5' fill='!0!' rx='1' ry='1' />" +
+                            "<rect x='5.5' y='0.5' width='4.5' height='4.5' fill='!1!' rx='1' ry='1' />" +
+                            "<rect x='0.5' y='5.5' width='4.5' height='4.5' fill='!2!' rx='1' ry='1' />" +
+                            "<rect x='5.5' y='5.5' width='4.5' height='4.5' fill='!3!' rx='1' ry='1' />" +
+                            "</svg>").replace(/!(\d+)!/g, function(_, m) { return colors[+m].color; }
+                        )));
+                    $('<div class="ktane-highlight-btn element">')
+                        .data('mode', 'element')
+                        .css({ right: nextIconPos() })
+                        .appendTo(document.body)
+                        .append($(penSvg.replace(/\{0\}/g, '')));
                     if ($('td, th').length)
                     {
-                        $('<div class="ktane-highlight-btn row">').data('mode', 'row').css({ right: 170 }).appendTo(document.body)
+                        $('<div class="ktane-highlight-btn row">')
+                            .data('mode', 'row')
+                            .css({ right: nextIconPos() })
+                            .appendTo(document.body)
                             .append($(penSvg.replace(/\{0\}/g, "<path d='M 0 600 0 400 1000 400 1000 600 z' class='ktane-highlight-pen'/>")));
-                        $('<div class="ktane-highlight-btn column">').data('mode', 'column').css({ right: 225 }).appendTo(document.body)
+                        $('<div class="ktane-highlight-btn column">')
+                            .data('mode', 'column')
+                            .css({ right: nextIconPos() })
+                            .appendTo(document.body)
                             .append($(penSvg.replace(/\{0\}/g, "<path d='M 400 0 600 0 600 1000 400 1000 z' class='ktane-highlight-pen'/>")));
                     }
                 }
 
+                // Draw button
+                $('<div class="ktane-highlight-btn draw">')
+                    .data('mode','draw')
+                    .css({right: nextIconPos()})
+                    .appendTo(document.body)
+                    .append(
+                        $('<svg viewBox="0 0 938 822" fill="none" xmlns="http://www.w3.org/2000/svg" class="ktane-highlight-svg"> <path class="ktane-highlight-pen" d="M378.5 446.494C426.602 468.915 458.654 492.087 493.5 553.494M278.5 599.201C312 612.701 327 629.201 325.5 666.201C582.334 483.605 713.321 350.451 916 39.7014C921.658 31.0271 905.687 15.5636 897 21.2014C576.595 229.139 445.807 356.741 278.5 599.201ZM20 712.201C19.999 819.715 335.5 866.201 308 638.201C171 530.701 120 689.201 80.9999 702.201C31.4918 698.221 20.0001 700.201 20 712.201Z" stroke="black" stroke-width="30"/></svg>')
+                    )
+
                 $('.ktane-highlight-btn').click(function()
                 {
                     let newMode = $(this).data('mode');
-                    if (newMode === "theme") {
-                        const checkbox = $('#dark-mode-enabled');
-                        checkbox.prop('checked', !checkbox.prop('checked'));
-                        updateDarkMode();
-                    } else if (newMode === 'color')
-                        setColor((currentColor + 1) % colors.length);
-                    else
-                    {
-                        $('.ktane-highlight-btn').removeClass('selected');
-                        if (newMode === mode)
-                            mode = null;
-                        else
-                        {
-                            mode = newMode;
-                            $(this).addClass('selected');
-                        }
+                    const drawingCheckbox = $('#freeform-drawing-enabled');
+                    switch(newMode) {
+                        case "theme":
+                            const darkModeCheckbox = $('#dark-mode-enabled');
+                            darkModeCheckbox.prop('checked', !darkModeCheckbox.prop('checked'));
+                            updateDarkMode();
+                            break;
+                        case "color":
+                            setColor((currentColor + 1) % colors.length);
+                            break;
+                        case "draw":
+                            const enablingDraw = !drawingCheckbox.prop('checked')
+                            drawingCheckbox.prop('checked', enablingDraw).trigger('change');
+                            $('.ktane-highlight-btn').removeClass('selected');
+                            if (enablingDraw) {
+                                mode = null;
+                                $(this).addClass('selected');
+                            }
+                            break;
+                        default:
+                            $('.ktane-highlight-btn').removeClass('selected');
+                            if (drawingCheckbox.prop('checked')) {
+                                drawingCheckbox.prop('checked', false).trigger('change');
+                            }
+                            if (newMode === mode)
+                                mode = null;
+                            else {
+                                mode = newMode;
+                                $(this).addClass('selected');
+                            }
                     }
                     return false;
                 });
@@ -577,7 +625,8 @@ if (!new URLSearchParams(window.location.search).has("merger")) {
                 left: 0,
                 pointerEvents: 'none',
                 zIndex: 9000,
-                opacity: 0.4
+                opacity: 0.4,
+                touchAction: 'none'
             }).appendTo(document.body)[0];
             drawCanvas.addEventListener('contextmenu', e => {
                 if (drawEnabled()) e.preventDefault();
@@ -700,7 +749,7 @@ if (!new URLSearchParams(window.location.search).has("merger")) {
                 // Setups current stroke with its color and width.
                 drawCtx.globalCompositeOperation = s.erase ? 'destination-out' : 'source-over';
                 drawCtx.strokeStyle = s.color || '#000';
-                drawCtx.lineWidth = s.width;
+                drawCtx.lineWidth = s.erase ? s.width*2 : s.width;
                 drawCtx.lineCap = 'round';
                 drawCtx.lineJoin = 'round';
             }
@@ -735,6 +784,10 @@ if (!new URLSearchParams(window.location.search).has("merger")) {
                     )
                 }
                 drawCanvas.style.cursor = on ? 'crosshair' : '';
+                $('.ktane-highlight-btn.draw').toggleClass('selected', on);
+                if (mobileControls) {
+                    $('html, body').css('overflow', on ? 'hidden' : '');
+                }
                 const activeEvents = on && !modifiersHeld
                 drawCanvas.style.pointerEvents = activeEvents ? 'auto' : 'none';
             }
