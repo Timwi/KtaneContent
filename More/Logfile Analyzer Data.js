0a86c1e01d1fdbd3a22401141ea614c24c099fcc
thunder725 <64666816+thunder725@users.noreply.github.com>
2026-09-23 03:08:23 +0200

    Giants Cipher LFA (#1861)
    * By thunder725

--- More/Logfile Analyzer Data.js
+++ More/Logfile Analyzer Data.js
@@ -8983,6 +8983,97 @@ let parseData = [
 		]
 	},
 	{
+		moduleID: "GiantsCipher",
+		loggingTag: "Giants Cipher",
+		matches: [
+			{
+				regex: /=-= Beginning Cipher (.) =-=/,
+				handler: function (matches, module) {
+					var dropdown = [`=-= Cipher ${matches[1]} =-=`, []];
+					var line = readLine();
+
+					while (true)
+					{
+						// Handle the fake Logs that start with <Giants Cipher #1> instead of [Giants Cipher #1]
+						if (line[0] == '<'){
+							line = readLine();
+							continue;
+						}
+
+						// Remove the starting tag
+						line = /^[ \t]*\[(?:Assets\.Scripts\.(?:\w+\.)+)?.+?\] ?(.+)/.exec(line)[1];
+						
+						if (/=-= Beginning Cipher (.) =-=/.test(line) || line == "Press this pattern to solve the module:")
+						{
+							linen --; // Read line is a new cipher, or the end of the module. Go back up once
+							break;
+						}
+						else if (/^[.X]{5}/.test(line)){
+							const grid = [line];
+							grid.push(...readTaggedLines(2)); // transform the array into individual elements then add them
+							
+							const svg = $SVG('<svg class="giants-cipher" viewbox="0 0 5 3">');
+							for (var x = 0; x < 3; x ++){
+								for (var y = 0; y < 5; y ++){
+									var lit = grid[x][y] == "." ? "dark" : "light";
+									const rect = $SVG(`<rect x=${y} y=${x} width=1 height=1>`).appendTo(svg);
+									const circle = $SVG(`<circle class=${lit} cx=${y}.5 cy=${x}.5 r="0.4"`).appendTo(svg);
+								}
+							}
+							dropdown[1].push(svg);
+						}
+						else if (/[.X]{5,}/.test(line)){ 
+							var regexMatch = /[.X]{5,}/.exec(line);
+							var grid = regexMatch[0];
+							var gridLength = grid.length;
+
+							const svg = $SVG(`<svg class="giants-cipher inline" viewbox="0 0 ${gridLength} 1">`);
+							for (var x = 0; x < gridLength; x ++){
+								var lit = grid[x] == "." ? "dark" : "light";
+								const rect = $SVG(`<rect x=${x} y=0 width=1 height=1>`).appendTo(svg);
+								const circle = $SVG(`<circle class=${lit} cx=${x}.5 cy=0.5 r="0.4"`).appendTo(svg);
+							}
+							
+							// Wrap line into a Span to make it an object and allow inlined SVGs
+							var lineParts = line.split(grid);
+							var newLine = $('<span>').html(lineParts[0] + svg[0].outerHTML + lineParts[1]);
+							dropdown[1].push(newLine);
+						}
+						else {
+							dropdown[1].push(line);
+						}
+						line = readLine();
+					}
+
+					module.push(dropdown);
+					return true;
+				}
+			},
+			{
+				regex: /^[.X]/,
+				handler: function (matches, module) {
+					const colors = {"." : "#181818", "X" : "#e0e0b8"};
+
+					const grid = [matches.input];
+					grid.push(...readTaggedLines(2)); // transform the array into individual elements then add them
+
+					const svg = $SVG('<svg class="giants-cipher" viewbox="0 0 5 3">');
+					for (var x = 0; x < 3; x ++){
+						for (var y = 0; y < 5; y ++){
+							var value = grid[x][y];
+							var lit = grid[x][y] == "." ? "dark" : "light";
+							const rect = $SVG(`<rect x=${y} y=${x} width=1 height=1>`).appendTo(svg);
+							const circle = $SVG(`<circle class=${lit} cx=${y}.5 cy=${x}.5 r="0.4"`).appendTo(svg);
+						}
+					}
+					module.push({obj: svg});
+					return true;
+				}
+			},
+			{ regex: /.+/ }
+		]
+	},
+	{
 		moduleID: "giantsDrink",
 		loggingTag: "The Giant's Drink",
 		displayName: "The Giant’s Drink"
